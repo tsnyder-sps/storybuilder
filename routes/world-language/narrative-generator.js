b54f084c1efefd2b99f83d5892ed51c694c41d7e
Ryan Marshall <rmarshall@scarboroughschools.org>
2025-12-18 16:08:49 -0500

    feat: implement narrative generator and update dashboard links

--- routes/world-language/narrative-generator.js
+++ routes/world-language/narrative-generator.js
@@ -13,6 +13,14 @@ const openai = new OpenAI({
   },
 });
 
+// Handle "GET /world-language/narrative-generator"
+router.get("/", (req, res) => {
+  res.render("world-language/narrative-generator", {
+    title: "Narrative Generator",
+    page: "world-language",
+  });
+});
+
 //Matthew's narrative quiz generation endpoints
 router.post("/quiz", async (req, res) => {
   const promptReq = req.body.prompt;
@@ -64,20 +72,31 @@ router.post("/quiz", async (req, res) => {
 
 // Matthew's narrative generation endpoint
 router.post("/narrative", async (req, res) => {
-  const promptReq = req.body.prompt;
+  console.log("Received narrative generation request:", req.body);
+  const vocabReq = req.body.vocab || "";
+  const tenseReq = req.body.tense || "present";
+  const additionalReq = req.body.additional || "";
+  const paragraphCountReq = req.body.paragraphCount || 5;
+  const languageReq = req.body.language || "French";
+  var fullPrompt = `Write me a creative narrative in ${languageReq} that features the following vocabulary words: ${vocabReq}. The narrative may only use the following verb tenses: ${tenseReq}. The narrative must be only ${paragraphCountReq} paragraphs long. Return only the narrative.`;
+  if (additionalReq != "") {
+    //user gave extra info
+    fullPrompt =
+      fullPrompt +
+      " Here are some additional instructions for the narrative: " +
+      additionalReq;
+  }
   try {
-    const { prompt = promptReq, max_tokens = 8192 } = req.body;
-
     // Validate the prompt
-    if (!prompt) {
+    if (!fullPrompt) {
       return res.status(400).json({ error: "Prompt is required." });
     }
 
     // // Call OpenAI API
     const completion = await openai.chat.completions.create({
       model: model,
-      max_tokens: max_tokens,
-      messages: [{ role: "user", content: prompt }],
+      max_tokens: 8192,
+      messages: [{ role: "user", content: fullPrompt }],
       stream: true,
       temperature: 2.0,
       min_p: 0.01,
